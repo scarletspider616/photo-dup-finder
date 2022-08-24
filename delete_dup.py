ad687b6005ad2957f7e97c7ebe98cf51579deb45
Joey Fallone <j.fallone@ualberta.ca>
2022-08-23 20:35:12 -0600

    Add the ability for the user to choose if they want to delete images or not

--- delete_dup.py
+++ delete_dup.py
@@ -9,10 +9,14 @@ def delete_duplicates(dups, delete_duplicate_update=None):
             delete_duplicate_update(count / total)
         delete_duplicate(hash, files)
 
+def delete_file(file):
+    send2trash.send2trash(file)
+
 def delete_duplicate(hash, files):
     print(f'Identified files for {hash}:')
     print(f'{files}')
     for file in files[1:]:
         # delete everything except the first one
         print(f'Sending {file} to trash....')
-        send2trash.send2trash(file)
+        delete_file(file)
+
--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 from contextlib import nullcontext
 import os
 from time import sleep
+import time
 import wx
 
 from find_dup import check_for_duplicates
-from delete_dup import delete_duplicate, delete_duplicates
+from delete_dup import delete_duplicate, delete_duplicates, delete_file
 
 
 class MainWindow(wx.Frame):
@@ -16,6 +17,9 @@ class MainWindow(wx.Frame):
     full_hash_progress = None
 
     delete_duplicate_progress = None
+
+    num_deletion_windows = 0
+    num_closed_deletion_windows = 0
 
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(600, 600))
@@ -55,18 +59,43 @@ class MainWindow(wx.Frame):
         duplicates = check_for_duplicates(self.path, size_hash_update=self.size_hash_update,
                                           small_hash_update=self.small_hash_update, full_hash_update=self.full_hash_update)
 
-        self.delete_duplicate_progress = wx.ProgressDialog('Deleting duplicaates in progress...', 'Please wait',
-                                                               maximum=100, parent=self, style=wx.PD_SMOOTH | wx.PD_AUTO_HIDE)
         if self.auto_delete_dup:
+            self.delete_duplicate_progress = wx.ProgressDialog('Deleting duplicaates in progress...', 'Please wait',
+                                                               maximum=100, parent=self, style=wx.PD_SMOOTH | wx.PD_AUTO_HIDE)
             delete_duplicates(
                 duplicates, delete_duplicate_update=self.delete_duplicate_update)
         else:
-            print('SHOW NOT YET SUPPORTED')
-            # for dup in duplicates:
-            #     # TODO get user input on if it should be deleted
-            #     delete_me = self.prompt_user_to_verify(dup.value())
-            #     if delete_me:
-            #         delete_duplicate(dup)
+            self.dups = [dups for _, dups in duplicates.items()]
+            self.prompt_for_deletion()
+            # for _, dups in duplicates.items():
+            #     windows = list()
+            #     for dup in dups:
+            #         windows.append(PromptForDeletionWindow(
+            #             None, 'Delete Photo?', dup))
+
+    def prompt_for_deletion(self):
+        if len(self.dups) == 0:
+            return
+        curr_dups = self.dups.pop()
+        self.num_deletion_windows = len(curr_dups)
+        self.num_closed_deletion_windows = 0
+        for dup_path in curr_dups:
+            window = PromptForDeletionWindow(None, 'Delete Photo?', dup_path)
+            window.Bind(wx.EVT_CLOSE, self.on_deletion_window_closed)
+            window.Show()
+
+    def on_deletion_window_closed(self, evt):
+        self.num_closed_deletion_windows += 1
+        evt.Skip()
+
+        if self.num_closed_deletion_windows == self.num_deletion_windows:
+            self.prompt_for_deletion()
+
+    def all_windows_closed(self, windows):
+        for window in windows:
+            if window.IsShown():
+                return False
+        return True
 
     # this is very hacky but this is just for fun mostly here
 
@@ -91,7 +120,38 @@ class MainWindow(wx.Frame):
 
     def delete_duplicate_update(self, curr_dec):
         self.delete_duplicate_progress.Update(int(curr_dec * 100))
-            
+
+
+def scale_bitmap(bitmap, width, height):
+    image = bitmap.ConvertToImage()
+    image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
+    result = wx.Bitmap(image)
+    return result
+
+
+class PromptForDeletionWindow(wx.Frame):
+    def __init__(self, parent, title, img_file):
+        wx.Frame.__init__(self, parent, title=title, size=(600, 600))
+        panel = wx.Panel(self)
+        self.img_file = img_file
+        bitmap = wx.Bitmap(img_file)
+        bitmap = scale_bitmap(bitmap, 300, 200)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        control = wx.StaticBitmap(panel, -1, bitmap)
+        control.SetPosition((0, 0))
+
+        delete_button = wx.Button(panel, -1, "Delete")
+        delete_button.Bind(wx.EVT_BUTTON, self.delete_file)
+
+        vbox.Add(control)
+        vbox.Add(delete_button)
+
+        panel.SetSizer(vbox)
+
+    def delete_file(self, evt):
+        print(f"USER CHOSE TO DELETE: {self.img_file}")
+        delete_file(self.img_file)
+        self.Close()
 
 
 app = wx.App(False)
